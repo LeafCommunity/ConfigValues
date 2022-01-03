7b5da177e0a637cc53f23cdee3947f86e4ba6cfa
RezzedUp <RezzedUp@users.noreply.github.com>
2022-01-03 12:12:17 -0600

    Update copyright to 2022.

--- config-values-bukkit/src/main/java/community/leaf/configvalues/bukkit/data/Load.java
+++ config-values-bukkit/src/main/java/community/leaf/configvalues/bukkit/data/Load.java
@@ -1,7 +1,14 @@
+/*
+ * Copyright © 2021-2022, RezzedUp <https://github.com/LeafCommunity/ConfigValues>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package community.leaf.configvalues.bukkit.data;
 
 public enum Load
 {
-	NOW,
-	LATER
+    NOW,
+    LATER
 }
